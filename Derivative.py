e81af85e79a3604226c630c730ec9ca5d05c77e3
Mathmatician <38021845+Mathmatician@users.noreply.github.com>
2020-07-19 18:34:54 -0600

    Update Derivative.py

--- Derivative.py
+++ Derivative.py
@@ -7,13 +7,13 @@ import math
 import numpy as np
 
 
-# Name: Takes derivative of received function from starting point to end point
+# Description: Takes derivative of received function from starting point to end point
 # Parameters: a function, FUNC, starting value start, ending value end, increment value DELTA, and the derivative values NUM_OF_dF (NUM_OF_dF determines the amount of times to take a derivative.)
 # Returns: two arrays - x values and y values, with spacings of DELTA increments
-def FunctionDerivative(FUNC, start, end, DELTA, NUM_OF_dF):
+def DerivativeOfFunction(FUNC, start, end, DELTA, NUM_OF_dF):
 
 
-    # Name: The derivative of the inquired function, f.
+    # Description: The derivative of the inquired function, F.
     # Parameters: a function, f, a floating point value x, and the derivative n (n determines the amount of times to take a derivative.)
     # Returns: a floating point value
     def dF(f, x, n):
@@ -40,3 +40,24 @@ def FunctionDerivative(FUNC, start, end, DELTA, NUM_OF_dF):
         i = i + delta;
 
     return x, y;
+
+
+
+
+
+
+
+# Description: The derivative of the input data
+# Parameters: the horizontal data, x_data, and the vertical data, y_data
+# Returns: two arrays - x values and y values, stored in x_data array and y data array respectively
+def DerivativeOfData(x_data, y_data):
+    if len(x_data) != len(y_data):
+        print('Both arrays must be the same length');
+        return 0, 0;
+
+    y = [];
+    for i in range(0, len(x_data) - 1):
+        m = (y_data[i + 1] - y_data[i]) / (x_data[i + 1] - x_data[i]);
+        y.append(m);
+
+    return x_data, y;
