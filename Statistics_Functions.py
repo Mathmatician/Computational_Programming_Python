fa9102f1ba4026336fa332f5b33b2881d89cb0e8
Mathmatician <38021845+Mathmatician@users.noreply.github.com>
2020-08-03 01:24:08 -0600

    Update Statistics_Functions.py

--- Statistics_Functions.py
+++ Statistics_Functions.py
@@ -4,6 +4,9 @@
 # Description: Different functions commonly used for Statistics (work in progress)
 
 import numpy as np
+import math
+
+
 
 
 # Description: Finds the average of a set of data points
@@ -39,7 +42,7 @@ def Median(data):
                 copied_data[i] = copied_data[i + 1];
                 copied_data[i + 1] = tmp;
     if N % 2 == 0:
-        i = N / 2;
+        i = int(N / 2);
         v = copied_data[i - 1] + copied_data[i];
         return v / 2;
     i = int(N / 2);
@@ -96,3 +99,123 @@ def Variance(data):
         lst.append(v);
     avg_dist = avg_dist / N;
     return lst, avg_dist;
+
+
+
+
+
+
+
+
+# Description: Finds the spread of the data using the variance principle
+# Parameters: an array of data points and a DELTA parameter specifying the range to be graphed from the center point of the Gaussian
+# Returns: an array of the variance spread for each data point and a floating point number that is the variance value of the data set
+def GaussianDistribution(data, DELTA):
+    tmp, v = Variance(data);
+    avg = Average(data);
+    std_dev = np.sqrt(v);
+
+    def GaussianFunction(x):
+        return (1 / (std_dev * np.sqrt(2*np.pi)) ) * np.exp( (-1/2)*( (x - avg)/std_dev )**2 );
+
+    x = [];
+    y = [];
+    x_0 = avg - DELTA;
+    x_f = avg + DELTA;
+    dx = 0.01;
+    i = x_0;
+    while i < x_f:
+        x.append(i);
+        y.append(GaussianFunction(i));
+        i = i + dx;
+    return x, y;
+
+
+
+
+
+
+
+
+# Description: Finds the spread of the data using the variance principle
+# Parameters: an array of data points, an initial point x_0, and an end point x_f
+# Returns: an array of the variance spread for each data point and a floating point number that is the variance value of the data set
+def GaussianBoundDistribution(data, x_0, x_f):
+    tmp, v = Variance(data);
+    avg = Average(data);
+    std_dev = np.sqrt(v);
+
+    def GaussianFunction(x):
+        return (1 / (std_dev * np.sqrt(2*np.pi)) ) * np.exp( (-1/2)*( (x - avg)/std_dev )**2 );
+
+    x = [];
+    y = [];
+    dx = 0.01;
+    i = x_0;
+    while i < x_f:
+        x.append(i);
+        y.append(GaussianFunction(i));
+        i = i + dx;
+    return x, y;
+
+
+
+
+
+
+
+
+# Description: Finds the spread of the data using the Poisson principle
+# Parameters: an array of data points, an initial point x_0, and an end point x_f
+# Returns: an array of the Poisson spread for each data point and a floating point number that is the variance value of the data set
+def PoissonBoundDistribution(data, x_0, x_f):
+    avg = Average(data);
+
+    def PoissonFunction(x):
+        numerator = EvaluateLargeExponent(avg, x);
+        denominator = StirlingApproximation(x);
+        return ( numerator / denominator ) * np.exp( -avg );
+
+    x = [];
+    y = [];
+    dx = 0.01;
+    i = x_0;
+    while i < x_f:
+        x.append(i);
+        y.append(PoissonFunction(i));
+        i = i + dx;
+    return x, y;
+
+
+
+
+
+
+
+
+# Description: Is used to approximate the factorial of the input parameter x
+# Parameters: any number (does not have to be integers only)
+# Returns: the approximate answer of x!
+def StirlingApproximation(x):
+    v = 0;
+    if v > 20:
+        v = EvaluateLargeExponent(x / math.e, x);
+    else:
+        v = (x / math.e)**x;
+    return np.sqrt(2 * np.pi * x) * v;
+
+
+
+
+
+
+
+
+# Description: Is used to evaluate large exponents
+# Parameters: base number, x, and exponenet, pwr
+# Returns: x^pwr (x**pwr)
+def EvaluateLargeExponent(x, pwr):
+    return 10**( pwr * np.log10(x) );
+
+
+
